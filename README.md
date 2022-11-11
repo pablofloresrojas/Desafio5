21f80891d57fa3ec609e697474d6e22803381497
PabloFlores <pflores@tecnigen.cl>
2022-11-10 23:48:18 -0300

    Desafio 5 Motores de Plantillas, handlebars, ejs, pug

---
+++ README.md
@@ -0,0 +1,3 @@
+Motores de Plantilla
+se me hizo natural trabajar con cualquiera de los 3 motores de plantilla, pero me quedo con Handlebars solo porque se me hizo mas comodo a 
+la hora de ver la sintaxis
---
+++ handlebars/server.js
@@ -0,0 +1,93 @@
+const express = require('express');
+const handlebars = require('express-handlebars');
+
+const { Router, text } = require('express');
+const Contenedor = require("./contenedor");
+
+const app = express();
+
+const PORT = 8080;
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+app.use(express.static("public"))
+
+
+app.engine("handlebars",handlebars.engine())
+
+app.set("view engine", "handlebars")
+app.set("views",__dirname+"/views")
+
+const manejador = new Contenedor("productos.txt");
+
+const routerProductos = Router();
+
+routerProductos.get("/",async(req,res)=>{
+    try {
+        const resp = await manejador.getAll();
+        res.status(resp.status).send(resp.data);
+    } catch (error) {
+        res.status(error.status).send(error.message);
+    }
+});
+
+routerProductos.get("/:id",async (req,res)=>{
+    try {
+        const resp = await manejador.getById(req.params.id);    
+        res.status(resp.status).json(resp.data)
+    } catch (error) {
+        res.status(error.status).send(error.message);
+    }
+    
+});
+
+routerProductos.post("/",async (req,res)=>{
+    try {
+        const resp = await manejador.save(req.body);
+        res.status(resp.status).json(resp.message)
+    } catch (error) {
+        res.status(error.status).send(error.message);
+    }
+})
+
+routerProductos.delete("/:id",async (req,res)=>{
+    //console.log("deleteProducto: ",req.params.id);
+    try {
+        const resp = await manejador.deleteById(req.params.id);
+        res.status(resp.status).send(resp.message);
+    } catch (error) {
+        res.status(error.status).send(error.message);
+    }
+});
+
+routerProductos.delete("/",async (req,res)=>{
+    //console.log("deleteProducto: ",req.params.id);
+    try {
+        const resp = await manejador.deleteAll();
+        res.status(resp.status).send(resp.message);
+    } catch (error) {
+        res.status(error.status).send(error.message);
+    }
+});
+
+app.use('/api/productos', routerProductos);
+
+app.post("/productos", async (req, res) => {
+    const resp = await manejador.save(req.body);
+    res.redirect('/')
+})
+app.get("/productos",async (req,res)=>{
+
+    const resp = await manejador.getAll();
+
+    console.log("productos: ",resp.data)
+
+    res.render("listado",{
+        productos: resp.data,
+        total: resp.data.length
+    })
+})
+
+app.listen( PORT, ()=>{
+    console.log(`Servidor escuchando el puerto: ${PORT}`);
+});
